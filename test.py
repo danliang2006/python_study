271f36e7252029d8049f7147a0e5ce9585403899
danliang <danliang2006@126.com>
2018-04-13 20:04:30 +0800

    python study

---
+++ test.py
@@ -0,0 +1,15 @@
+birthdays = {'a':'a1','b':'b1'}
+
+name = 'a'
+
+print(birthdays[name])
+
+for i in birthdays.values():
+    print(i)
+
+for x in    birthdays.keys():
+    print(x)
+
+for y in birthdays.items():
+    print(y)
+
